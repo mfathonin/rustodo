0f5e852d8380a2060675e73699966375f7cf4dae
mfathonin <mfathonin@gmail.com>
2024-04-15 14:01:03 +0700

    Adding interactivity using `dialoguer` create

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,35 @@ mod todo;
 mod todo_cli;
 
 use console::Term;
-use todo::{add_todo, remove_done, sort_todo};
+use todo::{remove_done, sort_todo};
 
 fn main() {
     let term = Term::stdout();
 
     let mut todos = Vec::new();
 
-    let mut is_loop = true;
-
     let _ = term.clear_screen();
 
-    let mut option = todo_cli::show_menus(&todos);
+    let mut option = todo_cli::show_menus(&mut todos);
 
-    while is_loop {
+    loop {
         if option == 0 {
             break;
         }
 
         let _ = term.clear_screen();
         match option {
-            1 => {
-                let todo = todo_cli::add_todo(&todos);
-                add_todo(todo, &mut todos);
-            }
-            2 => todo_cli::mark_todo(&mut todos),
-            3 => {
+            1 => todo_cli::mark_todo(&mut todos),
+            2 => {
                 sort_todo(&mut todos);
             }
-            4 => todos = remove_done(todos),
-            5 => todo_cli::delete_todo(&mut todos),
-            0 => is_loop = false,
+            3 => todos = remove_done(todos),
+            4 => todo_cli::delete_todo(&mut todos),
+            0 => break,
             _ => println!("\nWrong options"),
         }
 
         let _ = term.clear_screen();
-        option = todo_cli::show_menus(&todos);
+        option = todo_cli::show_menus(&mut todos);
     }
 }
